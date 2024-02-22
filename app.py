5f0eab09c7e4fef673d95a2ef9527140394b8042
PrincessKaPa <94419616+PrincessKaPa@users.noreply.github.com>
2024-02-22 10:15:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,9 @@ st.title("DrEaMiNg")
 st.header("Un espacio para crear y soñar")
 st.write("Utilizo esta app para crear fàcil y ràpidamente frondend y backend")
 image = Image.open('nubesmoradas.jpeg')
+
+st.image(image, caption="nubes moradas")
+
+
+
+
