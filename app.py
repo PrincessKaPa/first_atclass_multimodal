132f72edb96aad6a3f8741bf7f38c55884bfa97a
PrincessKaPa <94419616+PrincessKaPa@users.noreply.github.com>
2024-02-22 10:11:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ from PIL import Image
 
 st.title("¡Mi primera app!!") #Una app web que solo muestra un tìtulo
 st.title("DrEaMiNg")
+
+st.header("Un espacio para crear y soñar")
+st.write("Utilizo esta app para crear fàcil y ràpidamente frondend y backend")
+image = Image.open(
