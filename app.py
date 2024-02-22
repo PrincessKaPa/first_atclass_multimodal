e1e92269d3f59c2293a57030a5a90eaf53a5d3da
PrincessKaPa <94419616+PrincessKaPa@users.noreply.github.com>
2024-02-22 10:26:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,13 @@ st.image(image, caption="nubes moradas")
 texto = st.text_input("Escribe algo", "Este es mi texto")
 st.write("Esto es lo que escribiste: ", texto)
 
+st.subheader("Ahora usemos 2 columnas jeje")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Te presento la primera columna :)")
+  st.write("Las interfaces multomodales mejoran la experiencia de usuario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("¡Correcto!")
